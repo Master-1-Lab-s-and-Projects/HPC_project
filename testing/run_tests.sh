398753fd61bb1c9e16840fba010ace99cd97840a
RedMuggle <66295206+RedMuggle@users.noreply.github.com>
2021-05-25 15:54:06 +0200

    updated testing scripts

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -11,11 +11,29 @@ RESULTS="$2"
 
 for f in $(cat instances.list)
 do
-	#echo "Instances/$f"
-	echo "$f" >> $RESULTS
+    instance_name=$(echo $f | cut -d ',' -f 1)
+    nb_solutions_expected=$(echo $f | cut -d ',' -f 2)
+	echo "$instance_name" >> $RESULTS
+
+    echo "### $instance_name ###"
 	for i in $(seq 1 16)
 	do
-		#echo "$i"
-		mpirun -N $i "$PROGRAM" --in  "Instances/$f" --progress-report 0 | sed 's/FINI.*en \(.*\)s/\1/' >> $RESULTS
+        echo "$i/16"
+        res=$(mpirun -N $i "$PROGRAM" --in  "../Instances/$instance_name" --progress-report 0)
+        if [ $? -ne 0 ]
+        then
+            echo "Error" >> $RESULTS
+            continue
+        fi
+
+        nb_solutions=$(echo $res | sed 's/^.*Trouvé \([0-9]\+\) sol.*$/\1/')
+        if [ $nb_solutions -ne $nb_solutions_expected -a $nb_solutions -ne 0 ]
+        then
+            echo "Instance: $instance_name. Expected $nb_solutions_expected solutions, found $solutions !"
+            exit
+        fi
+
+        time=$(echo $res | sed 's/FINI.*en \(.*\)s/\1/')
+        echo $time >> $RESULTS
 	done
 done
